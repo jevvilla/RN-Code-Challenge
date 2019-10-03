c55f0b66901a8c7b54ad9a9760100ead4127c445
jevvilla <juan.es-19@hotmail.com>
2019-10-03 09:19:07 -0500

    reducers, actions, action types and provider implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
 
 import Router from './navigation';
+import store from './store';
 
 import colors from './common/colors';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar backgroundColor={colors.background} barStyle="light-content" />
       <Router />
-    </>
+    </Provider>
   );
 };
 
