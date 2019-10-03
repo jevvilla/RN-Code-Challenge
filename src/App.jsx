8fd123156b606a1a9230d59ddbb5c44cbf3e2158
jevvilla <juan.es-19@hotmail.com>
2019-10-03 08:11:26 -0500

    Router imported in App.jsx to display Overview as initial screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 
+import Router from './navigation';
+
 import colors from './common/colors';
 
 const App = () => {
   return (
     <>
       <StatusBar backgroundColor={colors.background} barStyle="light-content" />
+      <Router />
     </>
   );
 };
